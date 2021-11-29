70c507a9133d4cf4d0be474d0f9ea926f2b24ddd
Rodolfo Bertolino <rmbertolino@outlook.com>
2021-11-29 12:22:08 -0300

    add requeriments.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aesencrypter import EncryptString, DecryptString
 
-plain_string = "Hello, World!"
+plain_string = "Hello, World!!"
 secret_key = "9ka87c30-9889-77a4-24-7ce56a47-6718-41a4-8677-52fe438d4f7b"
 e = EncryptString(plain_string, secret_key)
 d = DecryptString(e, secret_key)
